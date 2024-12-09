db52920dd1a669275a48ef67ed8823b6d643a55e
THE KGM <keyurmoradiya46@gmail.com>
2024-12-09 21:05:31 +0530

    Added: home, and other sec ,and some styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./landing-page/Header";
+import Hero from "./landing-page/Hero";
+import About from "./landing-page/About";
+import Services from "./landing-page/Services";
+import Contact from "./landing-page/Contact";
+import Footer from "./landing-page/Footer";
+import "./landing-page/css/style.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/landing-page/Services/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { IoIosPricetags } from "react-icons/io";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { MdMiscellaneousServices } from "react-icons/md";
+
+const services = [
+  {
+    icon: <MdProductionQuantityLimits style={{ height: 40, width: 40 }} />,
+    title: "Premium Car Products for Every Journey",
+    desc: "Explore our wide range of car products designed to enhance your driving experience, from accessories to maintenance solutions, tailored for every car enthusiast.",
+  },
+  {
+    icon: <IoIosPricetags style={{ height: 40, width: 40 }} />,
+    title: "Affordable Pricing for Quality Car Products",
+    desc: "Get the best value for your money with competitive pricing on top-notch car products, ensuring quality without compromising your budget.",
+  },
+  {
+    icon: <MdMiscellaneousServices style={{ height: 40, width: 40 }} />,
+    title: "Reliable Car Services You Can Trust",
+    desc: "Experience expert car services tailored to your needs, from maintenance and repairs to upgrades, ensuring your vehicle performs at its best.",
+  },
+];
+function index() {
+  return (
+    <div className="py-20">
+      <div className="container-lg">
+        <div className="flex flex-col gap-10 items-center justify-between">
+          <div className="text-2xl italic">What we offer you!</div>
+          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full">
+            {services.map((item, index) => (
+              <div
+                className="border-2 min-h-40 flex flex-col justify-center  gap-3 px-5 py-5 rounded-lg"
+                key={index}
+              >
+                {item.icon}
+                <h5 className="font-bold">{item.title}</h5>
+                <p className="mt-3">{item.desc}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default index;
